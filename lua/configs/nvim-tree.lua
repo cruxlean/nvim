f38bd5c24ad30e343652a268e6c674ed420a0427
Smith adrian <fullspes.edu@outlook.com>
2022-08-20 20:28:17 +0800

    2022-08-20 Modify

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,5 +1,11 @@
 require("nvim-tree").setup(
     {
+        sync_root_with_cwd = true,
+        respect_buf_cwd = true,
+        update_focused_file = {
+            enable = true,
+            update_root = true
+        },
         -- 自动关闭
         -- auto_close = true,
         -- 视图
